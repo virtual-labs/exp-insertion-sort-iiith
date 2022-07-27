28b9198113346ee259614c615c64769b08c6d07c
Shashank Ramachandran <sr31@bu.edu>
2022-07-27 19:46:37 +0530

    Insertion sort

--- experiment/simulation/js/insert_demo.js
+++ experiment/simulation/js/insert_demo.js
@@ -7,13 +7,15 @@ class Insertion_sort{
       this.prev = -1;
       this.action = 0;
       this.fn_name = "";
-      this.card;
+      this.card = [];
       this.comparisons = 0;
       this.operation = "";
       this.interval = 0;
       this.num = [];
       this.index=0;
-      this.swaps =  0;
+      this.insertions =  0;
+      this.positions = [];
+      this.distances = {};
     };
   };
 let insertion_artefact = new Insertion_sort();
@@ -23,13 +25,8 @@ let insertion_artefact = new Insertion_sort();
   randomise();
   handlers()
 }
-anime({
-  targets: document.getElementsByClassName("card")[0],
-  translateY: [
-    {value : 100 , duration : 500},
-    {value: 0,duration :500}
-  ],
-});
+
+
 
 document.body.onload = function() {main_functions();}
 function handlers(){
@@ -40,6 +37,8 @@ function handlers(){
     document.getElementById("pause").onclick = function() {pause();};
     };
 
+    
+
 
 function randomise()
 { 
@@ -49,14 +48,24 @@ function randomise()
     insertion_artefact.num[i] = Math.floor(Math.random() * 90 + 10);
     var temp = document.createElement("div");
     temp.className = "card";
+    temp.id = `card${i}`;
     temp.innerHTML = insertion_artefact.num[i];
     temp.style.fontStyle = "normal";
     temp.style.color = "white";
     classToFill.appendChild(temp);
+    insertion_artefact.positions.push(i);
+    console.log(temp.id);
+    insertion_artefact.card.push(temp.id);
+   
+
+    
+   
   }
+  console.log(insertion_artefact.distances);
   document.getElementById("next").onclick = function() { start_sort(); };
   document.getElementById("pause").disabled = true;
   document.getElementById("pause").style.backgroundColor = "grey";
+
 };
 
 function change_interval()
@@ -78,8 +87,13 @@ function start_sort()
 {
   if(insertion_artefact.interval != 0) { clearInterval(insertion_artefact.interval); insertion_artefact.interval = 0; }
   document.getElementById("comment-box-bigger").style.visibility = "visible";
-  insertion_artefact.card = document.querySelectorAll('.card');
- 
+  //insertion_artefact.card = document.querySelectorAll('.card');
+  for(var i = 0 ; i< insertion_artefact.numOfCards;i++){
+    const dist = document.getElementById(`card${i}`).getBoundingClientRect();
+    insertion_artefact.distances[`card${i}`]=dist.left;
+  }
+  console.log(insertion_artefact.distances);
+  
   insertion_artefact.action = 1;
   insertion_artefact.finished = false;
   insertion_artefact.comparisons = 0;
@@ -87,6 +101,7 @@ function start_sort()
   insertion_artefact.iterator1 = 1;
   insertion_artefact.iterator2=1;
   insertion_artefact.operation = "Insert";
+  
 
   next_step();
 
@@ -114,6 +129,7 @@ function start_sort()
 
 //performs the next step pf the insertion
 function next_step(){
+  
   //console.log(insertion_artefact.action);
   resetcolor();
   if(insertion_artefact.iterator1===insertion_artefact.card.length){
@@ -130,9 +146,8 @@ function next_step(){
       insertion_artefact.action = -1;
     }else{
       if(insertion_artefact.iterator1>0){
-      document.getElementById("ins").innerHTML = "<p>" +  "Since " + insertion_artefact.card[insertion_artefact.iterator1].innerHTML +   " is greater than  "  + insertion_artefact.card[insertion_artefact.iterator1-1].innerHTML + "</p>" + "No Action is taken" + "</p>" +  "No of Comparisons : " + insertion_artefact.comparisons + "</p>" + "No of Swaps : " + insertion_artefact.swaps;
+      document.getElementById("ins").innerHTML = "<p>" +  "Since " +document.getElementById( insertion_artefact.card[insertion_artefact.iterator1]).innerHTML+   " is greater than  "  + document.getElementById(insertion_artefact.card[insertion_artefact.iterator1-1]).innerHTML + "</p>" + "No Action is taken" + "</p>" +  "No of Comparisons : " + insertion_artefact.comparisons + "</p>" + "No of insertions : " + insertion_artefact.insertions;
       }
-      insertion_artefact.iterator1=insertion_artefact.iterator2;
       insertion_sort();
     }
   }
@@ -140,72 +155,176 @@ function next_step(){
   {
     
     insertion_artefact.action = 1;
-    console.log(insertion_artefact.iterator1,insertion_artefact.index);
+    //console.log(insertion_artefact.iterator1,insertion_artefact.index);
     insertion(insertion_artefact.iterator1,insertion_artefact.index);
-    insertion_artefact.iterator1=insertion_artefact.index;
     insertion_artefact.finished = false;
-    //insertion_sort();
+    insertion_sort();
   }
 }
 
 function resetcolor(){
  
   for(var n = 0; n < insertion_artefact.numOfCards; n++){
-    insertion_artefact.card[n].style.backgroundColor =  "#288ec8";
+    document.getElementById(insertion_artefact.card[n]).style.backgroundColor =  "#288ec8";
   }
 }
 
+function clearCanvas(){
+  ctx = document.getElementById("myCanvas").getContext("2d");
+  ctx.clearRect(0,0,1200,50);
 
+}
 
 
 
 //finds the right index to insert the element 
 function insertionIndex(i){
+  clearCanvas();
+ 
   if(i===0){
     return 0;
   }
-  //for(let j = i-1;j>=0;j--){
-    let j = i-1;
-    console.log(insertion_artefact.card[j].innerHTML);
-    document.getElementById("ins").innerHTML = "<p>" +  "Comparing " + insertion_artefact.card[i].innerHTML + " with " + insertion_artefact.card[j].innerHTML + "</p>" + "No of Comparisons : " + insertion_artefact.comparisons + "</p>" + "No of Swaps : " + insertion_artefact.swaps;
+  for(let j = i-1;j>=0;j--){
+   
+    //console.log(insertion_artefact.card[j].innerHTML);
+    document.getElementById("ins").innerHTML = "<p>" +  "Comparing " + document.getElementById(insertion_artefact.card[i]).innerHTML + " with " + document.getElementById(insertion_artefact.card[j]).innerHTML + "</p>" + "No of Comparisons : " + insertion_artefact.comparisons + "</p>" + "No of insertions : " + insertion_artefact.insertions;
 
     resetcolor();
     insertion_artefact.comparisons++;
    
-    insertion_artefact.card[i].style.backgroundColor =  "#a4c652";
-    insertion_artefact.card[j].style.backgroundColor =   "#a4c652";
-    if(eval(insertion_artefact.card[i].innerHTML)<eval(insertion_artefact.card[j].innerHTML)){
+    document.getElementById(insertion_artefact.card[i]).style.backgroundColor =  "#a4c652";
+    document.getElementById(insertion_artefact.card[j]).style.backgroundColor =   "#a4c652";
+    if(j===0 && eval(document.getElementById(insertion_artefact.card[i]).innerHTML)<eval(document.getElementById(insertion_artefact.card[j]).innerHTML)){
      
-      return j;
+      return 0;
 
     }
     
-    if(eval(insertion_artefact.card[i].innerHTML)>=eval(insertion_artefact.card[j].innerHTML)){
+    if(eval(document.getElementById(insertion_artefact.card[i]).innerHTML)>=eval(document.getElementById(insertion_artefact.card[j]).innerHTML)){
      
-      return i;
+      return j+1;
     }
    
-//}
+}
   
+}
+
+
+
+
+function drawArrowhead(locx, locy, angle, sizex, sizey) {
+
+  var hx = sizex / 2;
+  var hy = sizey / 2;
+
+  ctx.translate((locx ), (locy));
+  ctx.rotate(angle);
+  ctx.translate(-hx,-hy);
+
+  ctx.beginPath();
+  ctx.moveTo(0,0);
+  ctx.lineTo(0,1*sizey);    
+  ctx.lineTo(1*sizex,1*hy);
+  ctx.closePath();
+  ctx.fill();
+
+  ctx.translate(hx,hy);
+  ctx.rotate(-angle);
+  ctx.translate(-locx,-locy);
+}       
+
+function findAngle(sx, sy, ex, ey) {
+  // make sx and sy at the zero point
+  return Math.atan2((ey - sy), (ex - sx));
+}
+
+function drawArrow(i,j){
+  const element1 = document.getElementById(insertion_artefact.card[i]);
+  const element2 = document.getElementById(insertion_artefact.card[j]);
+  const pos1 =  element1.getBoundingClientRect();
+  const pos2 =   element2.getBoundingClientRect();
+  const distance = pos1.x-pos2.x;
+  const dis1 = document.getElementById(insertion_artefact.card[0]).getBoundingClientRect();
+  const dis2 =document.getElementById(insertion_artefact.card[1]).getBoundingClientRect();
+  const distance2 = dis2.x - dis1.x;
+  const midx_boxi = pos1.x+30
+  const midy_boxi = 0;
+  const midx_boxj = pos2.x+30
+  const midy_boxj = 10;
+  const arrow_curvature = 90;
+  const angle = findAngle(0,0,midx_boxj,10)
+  const arrowhead_sizex = 15;
+  const arrowhead_sizey = 15;
+  ctx = document.getElementById("myCanvas").getContext("2d");
+  ctx.beginPath();
+  ctx.fillStyle = "rgba(55, 217, 56)";
+  ctx.moveTo(midx_boxi,midy_boxi);
+  ctx.quadraticCurveTo(midx_boxi,arrow_curvature, midx_boxj,midy_boxj);
+  ctx.stroke();
+  if(distance > (5*distance2)){
+  console.log("inside this if condition");
+  const angle2 = findAngle(0,0,midx_boxj,10)
+  drawArrowhead(midx_boxj+5,midy_boxj,angle2 ,arrowhead_sizex,arrowhead_sizey);
+  }else{
+    console.log("inside else considtion");
+    drawArrowhead(midx_boxj+3,midy_boxj,angle ,arrowhead_sizex,arrowhead_sizey);
+  }
 }
 
 //inserts the element into the right index 
 function insertion(i,j){
-
   resetcolor();
-  insertion_artefact.swaps++;
-  document.getElementById("ins").innerHTML = "<p>" + "Since " + insertion_artefact.card[i].innerHTML + " is lesser than " +  insertion_artefact.card[j].innerHTML + "</p>"+ "Inserting " + insertion_artefact.card[i].innerHTML +   " before "  + insertion_artefact.card[j].innerHTML + "</p>" +  "No of Comparisons : " + insertion_artefact.comparisons + "</p>" + "No of Swaps : " + insertion_artefact.swaps;
-  insertion_artefact.card[j].style.backgroundColor =  "#FFA500";
-  let temp = eval(insertion_artefact.card[i].innerHTML);
+  document.getElementById("ins").innerHTML = "<p>" + "Since " + document.getElementById(insertion_artefact.card[i]).innerHTML + " is lesser than " +  document.getElementById(insertion_artefact.card[j]).innerHTML + "</p>"+ "Inserting " + document.getElementById(insertion_artefact.card[i]).innerHTML +   " before "  + document.getElementById(insertion_artefact.card[j]).innerHTML + "</p>" +  "No of Comparisons : " + insertion_artefact.comparisons + "</p>" + "No of insertions : " + insertion_artefact.insertions;
+  document.getElementById(insertion_artefact.card[j]).style.backgroundColor =  "#FFA500";
+  drawArrow(i,j);
+  insertion_artefact.insertions++;
+  document.getElementById("ins").innerHTML = "<p>" + "Since " + document.getElementById(insertion_artefact.card[i]).innerHTML + " is lesser than " + document.getElementById(insertion_artefact.card[j]).innerHTML + "</p>"+ "Inserting " + document.getElementById(insertion_artefact.card[i]).innerHTML +   " before "  + document.getElementById(insertion_artefact.card[j]).innerHTML + "</p>" +  "No of Comparisons : " + insertion_artefact.comparisons + "</p>" + "No of insertions : " + insertion_artefact.insertions;
+  document.getElementById(insertion_artefact.card[j]).style.backgroundColor =  "#FFA500";
+  let temp = eval(document.getElementById(insertion_artefact.card[i]).innerHTML);
   
-  /**for(var n = i-1;n>=j;n--){
-    insertion_artefact.card[n+1].innerHTML = eval(insertion_artefact.card[n].innerHTML);
-  }**/
-  insertion_artefact.card[i].innerHTML = insertion_artefact.card[j].innerHTML;
-  insertion_artefact.card[j].innerHTML = temp;
+  for(var n = i-1;n>=j;n--){
+    document.getElementById(insertion_artefact.card[n+1]).innerHTML = eval(document.getElementById(insertion_artefact.card[n]).innerHTML);
+  }
+  document.getElementById(insertion_artefact.card[j]).innerHTML = temp;
   
 
+
+
+
+
+
+
+  
+  // console.log(distance);
+  // console.log(insertion_artefact.card[i].innerHTML,insertion_artefact.card[j].innerHTML)
+  // console.log(insertion_artefact.distances);
+  //  anime({
+  //   targets: document.getElementById(insertion_artefact.card[i]),
+  //   translateY: [
+  //     {value : 30 , duration : 500},
+  //     {value : 0 , duration : 500,delay:250}
+  //   ],
+  //   /**translateX: [
+  //     {value: -distance ,duration : 500 }
+  //   ],**/
+      
+  //  });
+
+  // //insertion_artefact.distances[insertion_artefact.card[i]] -= distance;
+  // for(var n = i-1;n>=j;n--){
+  //   const posn =  document.getElementById(insertion_artefact.card[n]).getBoundingClientRect();
+  //   console.log(document.getElementById(insertion_artefact.card[n]),distance2);
+  //   anime({
+  //     targets:  document.getElementById(insertion_artefact.card[n]),
+  //     translateX : [
+  //       {value : distance2 ,duration : 500}
+  //     ]
+  // });
+  // //insertion_artefact.distances[insertion_artefact.card[n]] += distance2;
 }
+
+
+
 
 function pause(){
   if(insertion_artefact.prev == -1){
@@ -226,17 +345,17 @@ function insertion_sort()
 {
   if(insertion_artefact.finished!==true){
     insertion_artefact.iterator1++;
-    insertion_artefact.iterator2 = insertion_artefact.iterator1;
   }
   
   else
   { 
+      clearCanvas();
       if(document.getElementById("interval").value != 100)
       {
         clearInterval(insertion_artefact.interval);
         insertion_artefact.interval = 0;
       }
-      document.getElementById("ins").innerHTML = "<h3>The sort is complete - there were " + insertion_artefact.comparisons + " comparisons and  " + insertion_artefact.swaps + " swaps "
+      document.getElementById("ins").innerHTML = "<h3>The sort is complete - there were " + insertion_artefact.comparisons + " comparisons and  " + insertion_artefact.insertions + " insertions "
 
       document.getElementById("next").style.backgroundColor = "grey";
       document.getElementById("next").disabled = true;
--- experiment/simulation/js/insert_exercise.js
+++ experiment/simulation/js/insert_exercise.js
@@ -1,38 +1,67 @@
 class Insertion_sort{
-  constructor(){
-    this.iterator1 = 0;
-    this.finished = false;
-    this.numOfCards = 10;
-    this.prev = -1;
-    this.action = 0;
-    this.fn_name = "";
-    this.card = [];
-    this.comparisons = 0;
-    this.operation = "";
-    this.interval = 0;
-    this.num = [];
-    this.index=0;
-    this.insertions =  0;
-    this.positions = [];
-    this.distances = {};
+    constructor(){
+      this.iterator1 = 0;
+      this.iterator2 = 0;
+      this.finished = false;
+      this.numOfCards = 10;
+      this.prev = -1;
+      this.action = 0;
+      this.fn_name = "";
+      this.card = [];
+      this.comparisons = 0;
+      this.operation = "";
+      this.interval = 0;
+      this.num = [];
+      this.index=0;
+      this.insertions =  0;
+      this.positions = [];
+      this.distances = {};
+      this.question = Math.floor(Math.random() * 5 + 1);
+      this.targetinsertions = Math.floor(Math.random()*9+1);
+
+    };
   };
-};
 let insertion_artefact = new Insertion_sort();
 
-
   function main_functions()
-  { 
-    randomise();
-    start_sort();
-    handlers()
-  }
+{ 
+  randomise();
+  handlers()
+  makeQuestion();
+}
+
+
 
 document.body.onload = function() {main_functions();}
 function handlers(){
-  document.getElementById("next").onclick = function() {check_next();};
-  document.getElementById("swap").onclick = function() {check_swap();};
-  document.getElementById("reset").onclick = function() {reload();};
+    document.getElementById("next").onclick = function() {submit()};
+    document.getElementById("reset").onclick = function() {reload();};
     };
+
+
+
+function makeQuestion(){
+  if(insertion_artefact.targetinsertions===1){
+    document.getElementById("question").innerHTML = "<b>Question :</b> Find the " + insertion_artefact.targetinsertions + "st element in the array after " + insertion_artefact.question + " insertions";
+  }
+  else if(insertion_artefact.targetinsertions===2){
+    document.getElementById("question").innerHTML = "<b>Question :</b> Find the " + insertion_artefact.targetinsertions + "nd element in the array after " + insertion_artefact.question + " insertions";
+  }
+  else if(insertion_artefact.targetinsertions===3){
+    document.getElementById("question").innerHTML = "<b>Question :</b> Find the " + insertion_artefact.targetinsertions+ "rd element in the array after " + insertion_artefact.question + " insertions";
+  }
+  else if(insertion_artefact.targetinsertions===4){
+    document.getElementById("question").innerHTML = "<b>Question :</b> Find the " + insertion_artefact.targetinsertions + "rth element in the array after " + insertion_artefact.question + " insertions";
+  }else{
+    document.getElementById("question").innerHTML = "<b>Question :</b> Find the " + insertion_artefact.targetinsertions+ "th element in the array after " + insertion_artefact.question + " insertions";
+
+  }
+
+
+  
+
+}
+
 
 function randomise()
 { 
@@ -50,13 +79,131 @@ function randomise()
     insertion_artefact.positions.push(i);
     console.log(temp.id);
     insertion_artefact.card.push(temp.id);
-    
+  }
+  // console.log(insertion_artefact.distances);
+  // document.getElementById("next").onclick = function() { start_sort(); };
+};
 
-    
-    
+function start_sort()
+{
+  if(insertion_artefact.interval != 0) { clearInterval(insertion_artefact.interval); insertion_artefact.interval = 0; }
+  document.getElementById("comment-box-bigger").style.visibility = "visible";
+  //insertion_artefact.card = document.querySelectorAll('.card');
+  for(var i = 0 ; i< insertion_artefact.numOfCards;i++){
+    const dist = document.getElementById(`card${i}`).getBoundingClientRect();
+    insertion_artefact.distances[`card${i}`]=dist.left;
+  }
+  console.log(insertion_artefact.distances);
+  
+  insertion_artefact.action = 1;
+  insertion_artefact.finished = false;
+  insertion_artefact.comparisons = 0;
+  insertion_artefact.fn_name = "insertion_sort"
+  insertion_artefact.iterator1 = 1;
+  insertion_artefact.iterator2=1;
+  insertion_artefact.operation = "Insert";
+  
+
+  next_step();
+
+  document.getElementById("next").onclick = function() { next_step(); };
+  document.getElementById("next").value = "Next";
+ 
+  if(document.getElementById("interval").value == 100)
+  {
+    document.getElementById("next").disabled = false;
+  }
+  else
+  {
+    if(insertion_artefact.interval == 0)
+      insertion_artefact.interval = setInterval(next_step,2600-document.getElementById("interval").value);
+    else
+    {
+      clearInterval(insertion_artefact.interval);
+      insertion_artefact.interval = 0;
+    }
   }
 };
+
+function insertionSorts(arr) {
+  let insertions = 0;
+  let swaps =0;
+  for (let i = 1; i < arr.length; i++) {
+    swaps = 0;
+    if(insertions===insertion_artefact.question){
+      break;
+    }
     
+    let currentValue = arr[i]
+    let j
+    for (j = i - 1; j >= 0 && arr[j] > currentValue; j--) {
+      swaps = 1;
+      arr[j + 1] = arr[j]
+    }
+    arr[j + 1] = currentValue
+    if(swaps ===1){
+      insertions ++
+    }
+    
+  }
+  return arr
+}
+
+
+function submit(){
+  start_sort();
+  var text1 = document.getElementById("textbox").value;
+  console.log(text1);
+  var answer_array = insertionSorts(insertion_artefact.num);
+  var answer = answer_array[insertion_artefact.targetinsertions-1];
+  console.log(answer_array,answer);
+  console.log(eval(text1),answer);
+  
+
+ if(eval(text1) === answer){
+  start_sort();
+  document.getElementById("ins").innerHTML =  "<b>This answer is correct! </b>"  
+ }else{
+  
+  document.getElementById("ins").innerHTML = "<b>This answer is wrong please try again!</b>"
+
+ }
+ }
+
+//performs the next step pf the insertion
+function next_step(){
+  //console.log(insertion_artefact.action);
+  resetcolor();
+  if(insertion_artefact.insertions===insertion_artefact.question){
+    insertion_artefact.finished=true;
+    insertion_sort();
+    return;
+  }
+  if(insertion_artefact.action == 1)
+  {
+    
+    insertion_artefact.index = insertionIndex(insertion_artefact.iterator1);
+    if(insertion_artefact.index!=insertion_artefact.iterator1){ 
+      
+      insertion_artefact.action = -1;
+    }else{
+      if(insertion_artefact.iterator1>0){
+      document.getElementById("ins").innerHTML = "<p>" +  "Since " +document.getElementById( insertion_artefact.card[insertion_artefact.iterator1]).innerHTML+   " is greater than  "  + document.getElementById(insertion_artefact.card[insertion_artefact.iterator1-1]).innerHTML + "</p>" + "No Action is taken" + "</p>" +  "No of Comparisons : " + insertion_artefact.comparisons + "</p>" + "No of insertions : " + insertion_artefact.insertions;
+      }
+      insertion_sort();
+    }
+  }
+  else
+  {
+    
+    insertion_artefact.action = 1;
+    //console.log(insertion_artefact.iterator1,insertion_artefact.index);
+    insertion(insertion_artefact.iterator1,insertion_artefact.index);
+    insertion_artefact.finished = false;
+    insertion_sort();
+  }
+}
+
 function resetcolor(){
  
   for(var n = 0; n < insertion_artefact.numOfCards; n++){
@@ -64,33 +211,15 @@ function resetcolor(){
   }
 }
 
-function start_sort()
-{
-  console.log("inside start sort");
-  document.getElementById("comment-box-smaller").style.visibility = "visible";
-  for(var i = 0 ; i< insertion_artefact.numOfCards;i++){
-    const dist = document.getElementById(`card${i}`).getBoundingClientRect();
-    insertion_artefact.distances[`card${i}`]=dist.left;
-  }
- 
-  insertion_artefact.action = 1;
-  insertion_artefact.finished = false;
-  insertion_artefact.comparisons = 0;
-  insertion_artefact.fn_name = "insertion_sort"
-  insertion_artefact.iterator1 = 1;
-  insertion_artefact.operation = "Insert";
+function clearCanvas(){
+  ctx = document.getElementById("myCanvas").getContext("2d");
+  ctx.clearRect(0,0,1200,50);
 
-  document.getElementById("swap").style.visibility = "visible";
-  document.getElementById("next").style.visibility = "visible";
-
-  document.getElementById("swap").onclick = function() { check_swap(); };
-  document.getElementById("next").onclick = function() { check_next(); };
-
-  document.getElementById("next").disabled = false;
-};
+}
 
 
 
+//finds the right index to insert the element 
 function insertionIndex(i){
   clearCanvas();
  
@@ -100,7 +229,7 @@ function insertionIndex(i){
   for(let j = i-1;j>=0;j--){
    
     //console.log(insertion_artefact.card[j].innerHTML);
-    //document.getElementById("ins").innerHTML = "<p>" +  "Comparing " + document.getElementById(insertion_artefact.card[i]).innerHTML + " with " + document.getElementById(insertion_artefact.card[j]).innerHTML + "</p>" + "No of Comparisons : " + insertion_artefact.comparisons + "</p>" + "No of insertions : " + insertion_artefact.insertions;
+    document.getElementById("ins").innerHTML = "<p>" +  "Comparing " + document.getElementById(insertion_artefact.card[i]).innerHTML + " with " + document.getElementById(insertion_artefact.card[j]).innerHTML + "</p>" + "No of Comparisons : " + insertion_artefact.comparisons + "</p>" + "No of insertions : " + insertion_artefact.insertions;
 
     resetcolor();
     insertion_artefact.comparisons++;
@@ -121,6 +250,9 @@ function insertionIndex(i){
 }
   
 }
+
+
+
 
 function drawArrowhead(locx, locy, angle, sizex, sizey) {
 
@@ -181,13 +313,6 @@ function drawArrow(i,j){
   }
 }
 
-
-function clearCanvas(){
-  ctx = document.getElementById("myCanvas").getContext("2d");
-  ctx.clearRect(0,0,1200,50);
-
-}
-
 //inserts the element into the right index 
 function insertion(i,j){
   resetcolor();
@@ -205,15 +330,8 @@ function insertion(i,j){
   document.getElementById(insertion_artefact.card[j]).innerHTML = temp;
   
 
-
-
-
-
-
-
-
-  //console.log(distance);
-  //console.log(insertion_artefact.card[i].innerHTML,insertion_artefact.card[j].innerHTML)
+  // console.log(distance);
+  // console.log(insertion_artefact.card[i].innerHTML,insertion_artefact.card[j].innerHTML)
   // console.log(insertion_artefact.distances);
   //  anime({
   //   targets: document.getElementById(insertion_artefact.card[i]),
@@ -240,45 +358,6 @@ function insertion(i,j){
   // //insertion_artefact.distances[insertion_artefact.card[n]] += distance2;
 }
 
-
-
-function check_swap(){
-  
-  var temp = insertionIndex(insertion_artefact.iterator1);
-  console.log(temp);
-  console.log(insertion_artefact.iterator1);
-  if(temp !== insertion_artefact.iterator1)
-  {
-    document.getElementById("ins").innerHTML = "Correct!";
-    console.log(insertion_artefact.iterator1,temp);
-    insertion(insertion_artefact.iterator1,temp);
-    insertion_artefact.iterator1=temp;
-    insertion_artefact.finished = false;
-    
-  }else{
-    document.getElementById("ins").innerHTML = "Incorrect, Think again!";    
-  
-  }
-};
-
-function check_next(){
-  if(insertion_artefact.iterator1===insertion_artefact.card.length){
-    insertion_artefact.finished=true;
-    insertion_sort();
-    return;
-  }
-  var temp = insertionIndex(insertion_artefact.iterator1);
-  
-  if(temp ===insertion_artefact.iterator1)
-  {
-    document.getElementById("ins").innerHTML = "Correct!";
-    insertion_sort();
-  }else{
-    document.getElementById("ins").innerHTML = "Incorrect, Think again!";   
-  } 
-};
-
-
 function insertion_sort()
 {
   if(insertion_artefact.finished!==true){
@@ -297,13 +376,17 @@ function insertion_sort()
 
       document.getElementById("next").style.backgroundColor = "grey";
       document.getElementById("next").disabled = true;
-      document.getElementById("pause").style.backgroundColor = "grey";
-      document.getElementById("pause").disabled = true;
+      //document.getElementById("pause").disabled = true;
       insertion_artefact.iterator1 = 0;
     
     
   }
 }
+
+function reload(){
+  location.reload(true);
+};
+
 
 
 
